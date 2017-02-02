51e71714559bea6101501f28ad5fb2c430c81799
chaojie <chaojie.keepgoing@gmail.com>
2017-02-02 17:14:26 -0500

    move to aws

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,21 +135,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'eft/static/'),
     os.path.join(BASE_DIR, 'static/')
 ]
-
-# where static files will be collected and save into
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_state_files')
-
-# config for heroku
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
